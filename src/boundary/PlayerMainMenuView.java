881e862ad5d50d468d4421f22c0ca145d9ce2284
Maurizio Vitale <mauriziovit@gmail.com>
2015-04-17 20:46:33 -0400

    Fix PlayerMainMenuView
    you’re welcome

--- src/boundary/PlayerMainMenuView.java
+++ src/boundary/PlayerMainMenuView.java
@@ -2,15 +2,17 @@ package boundary;
 
 import controller.ExitCtrl;
 import controller.PlayerLoadLevelSelectCtrl;
-import java.awt.Font;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.SwingConstants;
+import javax.swing.BoxLayout;
 import model.PlayerModel;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Component;
 
 /**
  * PlayerMainMenuView for the player application
@@ -18,64 +20,49 @@ import model.PlayerModel;
  * @author Bailey Sheridan
  */
 public class PlayerMainMenuView extends JPanel {
-  PlayerModel model;
-  PlayerApplication app;
+	PlayerModel model;
+	PlayerApplication app;
 
-  public PlayerMainMenuView(PlayerApplication app, PlayerModel model) {
-    this.app = app;
-    this.model = model;
+	public PlayerMainMenuView(PlayerApplication app, PlayerModel model) {
+		this.app = app;
+		this.model = model;
 
-    JLabel lblSixesWild = new JLabel("Sixes Wild");
-    lblSixesWild.setFont(new Font("Matura MT Script Capitals", Font.PLAIN, 26));
-    lblSixesWild.setHorizontalAlignment(SwingConstants.CENTER);
+		setMinimumSize(new Dimension(600,480));
+		setPreferredSize(new Dimension(600, 480));
 
-    JButton btnLevelSelect = new JButton("Level Select");
-    btnLevelSelect.addActionListener(new PlayerLoadLevelSelectCtrl(app, model));
-    btnLevelSelect.setFont(new Font("Lucida Bright", Font.PLAIN, 14));
+		GridBagLayout gridBagLayout = new GridBagLayout();
 
-    JButton btnNewButton = new JButton("Achievements");
-    btnNewButton.setFont(new Font("Lucida Bright", Font.PLAIN, 14));
+		setLayout(gridBagLayout);
 
-    JButton btnExit = new JButton("Exit");
-    btnExit.setFont(new Font("Lucida Bright", Font.PLAIN, 14));
-    btnExit.addActionListener(new ExitCtrl());
+		JPanel panelContent = new JPanel();
+		panelContent.setBorder(null);
+		panelContent.setSize(300,500);
 
-    ///////////////////
-    //**DON'T TOUCH**//
-    ///////////////////
-    GroupLayout groupLayout = new GroupLayout(this);
-    groupLayout.setHorizontalGroup(
-      groupLayout.createParallelGroup(Alignment.TRAILING)
-        .addGroup(groupLayout.createSequentialGroup()
-          .addGap(150)
-          .addComponent(btnLevelSelect, GroupLayout.PREFERRED_SIZE, 208, Short.MAX_VALUE)
-          .addGap(150))
-        .addGroup(groupLayout.createSequentialGroup()
-          .addGap(175)
-          .addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 158, Short.MAX_VALUE)
-          .addGap(175))
-        .addGroup(groupLayout.createSequentialGroup()
-          .addGap(215)
-          .addComponent(btnExit, GroupLayout.DEFAULT_SIZE, 78, Short.MAX_VALUE)
-          .addGap(215))
-        .addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
-          .addGap(180)
-          .addComponent(lblSixesWild, GroupLayout.PREFERRED_SIZE, 136, Short.MAX_VALUE)
-          .addGap(180))
-    );
-    groupLayout.setVerticalGroup(
-      groupLayout.createParallelGroup(Alignment.LEADING)
-        .addGroup(groupLayout.createSequentialGroup()
-          .addGap(20)
-          .addComponent(lblSixesWild, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE)
-          .addGap(50)
-          .addComponent(btnLevelSelect, GroupLayout.PREFERRED_SIZE, 63, GroupLayout.PREFERRED_SIZE)
-          .addGap(18)
-          .addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 39, GroupLayout.PREFERRED_SIZE)
-          .addPreferredGap(ComponentPlacement.RELATED, 55, Short.MAX_VALUE)
-          .addComponent(btnExit, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
-          .addContainerGap())
-    );
-    setLayout(groupLayout);
-  }
+		add(panelContent);
+		panelContent.setLayout(new BoxLayout(panelContent, BoxLayout.Y_AXIS));
+
+		JLabel lblSixesWild = new JLabel("Sixes Wild");
+		panelContent.add(lblSixesWild);
+		lblSixesWild.setFont(new Font("Matura MT Script Capitals", Font.PLAIN, 26));
+		lblSixesWild.setAlignmentX(Component.CENTER_ALIGNMENT);
+		lblSixesWild.setAlignmentY(Component.CENTER_ALIGNMENT);
+
+		JButton btnLevelSelect = new JButton("Level Select");
+		panelContent.add(btnLevelSelect);
+		btnLevelSelect.addActionListener(new PlayerLoadLevelSelectCtrl(app, model));
+		btnLevelSelect.setAlignmentX(Component.CENTER_ALIGNMENT);
+		btnLevelSelect.setAlignmentY(Component.CENTER_ALIGNMENT);
+
+
+		JButton btnNewButton = new JButton("Achievements");
+		panelContent.add(btnNewButton);
+		btnNewButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		btnNewButton.setAlignmentY(Component.CENTER_ALIGNMENT);
+
+		JButton btnExit = new JButton("Exit");
+		panelContent.add(btnExit);
+		btnExit.addActionListener(new ExitCtrl());
+		btnExit.setAlignmentX(Component.CENTER_ALIGNMENT);
+		btnExit.setAlignmentY(Component.CENTER_ALIGNMENT);
+	}
 }
