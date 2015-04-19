ad87b5e9f7f003f873830aaae8da0390ec864a93
Maurizio Vitale <mauriziovit@gmail.com>
2015-04-19 18:13:03 -0400

    Fix PlayerLevelView
    scales better now

--- src/boundary/PlayerLevelView.java
+++ src/boundary/PlayerLevelView.java
@@ -1,118 +1,103 @@
 package boundary;
 
 import controller.PlayerMainMenuCtrl;
-import java.awt.Font;
-import java.awt.GridLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.GroupLayout;
+import java.awt.Dimension;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.LayoutStyle.ComponentPlacement;
 import model.PlayerModel;
+import javax.swing.BoxLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.Component;
 
 public class PlayerLevelView extends JPanel {
-  /**
-   * The view when a player loads up a level.
-   * @author Bailey Sheridan
-   */
-  PlayerApplication app;
-  PlayerModel model;
-  public PlayerBoardView boardView;
+    /**
+        * The view when a player loads up a level.
+        * @author Bailey Sheridan & Maurizio Vitale & Eli Skeggs
+        */
+    PlayerApplication app;
+    PlayerModel model;
+    public PlayerBoardView boardView;
 
-  public PlayerLevelView(PlayerApplication app, PlayerModel model) {
-    this.app = app;
-    this.model = model;
+    public PlayerLevelView(PlayerApplication app, PlayerModel model) {
+        this.app = app;
+        this.model = model;
 
-    //I don't know how to initialize the board. Haven't tested.
-    boardView = new PlayerBoardView(app, model);
+        setMinimumSize(new Dimension(600,480));
+        setPreferredSize(new Dimension(600, 480));
+        setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+    
+        //Panel Left (Game Board)
+    
+        JPanel panelLeft = new JPanel();
+        panelLeft.setAlignmentY(0);
+        add(panelLeft);
+        panelLeft.setLayout(new BoxLayout(panelLeft, BoxLayout.X_AXIS));
+        boardView = new PlayerBoardView(app, model);
+        boardView.setAlignmentY(0);
+        panelLeft.add(boardView);
+        
+        //Panel Controls
 
-    JButton btnRestart = new JButton("Restart");
-    JButton btnMainMenu = new JButton("Main Menu");
-    btnMainMenu.addActionListener(new PlayerMainMenuCtrl(app, model));
+        JPanel panelControls = new JPanel();
+        panelControls.setAlignmentY(Component.TOP_ALIGNMENT);
+        panelLeft.add(panelControls);
+        panelControls.setLayout(new BoxLayout(panelControls, BoxLayout.Y_AXIS));
+        
+        JButton btnSwap = new JButton("Z");
+        panelControls.add(btnSwap);
+        
+        JButton btnRemove = new JButton("X");
+        panelControls.add(btnRemove);
+        
+        JButton btnScramble = new JButton("C");
+        panelControls.add(btnScramble);
+    
+        //Panel Right
+    
+        JPanel panelRight = new JPanel();
+        panelRight.setAlignmentX(0);
+        panelRight.setAlignmentY(0);
+        add(panelRight);
+        panelRight.setLayout(new BoxLayout(panelRight, BoxLayout.Y_AXIS));
+    
+        //Panel Navigation
+    
+        JPanel panelNavigation = new JPanel();
+        panelNavigation.setAlignmentX(0.0f);
+        panelNavigation.setAlignmentY(Component.BOTTOM_ALIGNMENT);
+        panelRight.add(panelNavigation);
+        panelNavigation.setLayout(new BoxLayout(panelNavigation, BoxLayout.X_AXIS));
+    
+        JButton btnRestart = new JButton("Restart");
+        btnRestart.addActionListener(new PlayerResetLevelCtrl(app,model));
+        panelNavigation.add(btnRestart);
+    
+        JButton btnMainMenu = new JButton("Main Menu");
+        btnMainMenu.addActionListener(new PlayerMainMenuCtrl(app, model));
+        panelNavigation.add(btnMainMenu);
+            
+                
+        //Panel Info
+                
+        JPanel panelInfo = new JPanel();
+        panelInfo.setAlignmentX(0);
 
-    JPanel panel_1 = new JPanel();
-    panel_1.setLayout(new GridLayout(0, 3, 0, 0));
+        panelRight.add(panelInfo);
+        panelInfo.setLayout(new BoxLayout(panelInfo, BoxLayout.Y_AXIS));
+                
+        JLabel lblScore = new JLabel("Score");
+        panelInfo.add(lblScore);
+                
+        JLabel lblScoreNum = new JLabel("12345");
+        panelInfo.add(lblScoreNum);
+                
+        JLabel lblCounter = new JLabel("Moves Left");
+        panelInfo.add(lblCounter);
+                
+        JLabel lblCounterNum = new JLabel("20");
+        panelInfo.add(lblCounterNum);
 
-    JButton button = new JButton("<->");
-    panel_1.add(button);
-
-    JButton button_1 = new JButton("?");
-    panel_1.add(button_1);
-
-    JButton button_2 = new JButton("??");
-    panel_1.add(button_2);
-
-    JPanel panel_2 = new JPanel();
-
-    JLabel lblScore = new JLabel("Score");
-    lblScore.setFont(new Font("Snap ITC", Font.PLAIN, 18));
-
-    JLabel lblNewLabel = new JLabel("12345");
-    lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 22));
-
-    JLabel lblMovesLeft = new JLabel("Moves Left");
-    lblMovesLeft.setFont(new Font("Snap ITC", Font.PLAIN, 18));
-
-    JLabel label = new JLabel("20");
-    label.setFont(new Font("Tahoma", Font.PLAIN, 22));
-
-    ////////////////////
-    //**DO NOT TOUCH**//
-    ////////////////////
-    GroupLayout groupLayout = new GroupLayout(this);
-    groupLayout.setHorizontalGroup(
-      groupLayout.createParallelGroup(Alignment.LEADING)
-        .addGroup(groupLayout.createSequentialGroup()
-          .addGap(10)
-          .addComponent(boardView, GroupLayout.PREFERRED_SIZE, 545, GroupLayout.PREFERRED_SIZE)
-          .addGap(34)
-          .addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
-            .addGroup(groupLayout.createSequentialGroup()
-              .addComponent(btnRestart, GroupLayout.PREFERRED_SIZE, 89, GroupLayout.PREFERRED_SIZE)
-              .addPreferredGap(ComponentPlacement.RELATED, 13, Short.MAX_VALUE)
-              .addComponent(btnMainMenu, GroupLayout.PREFERRED_SIZE, 99, GroupLayout.PREFERRED_SIZE))
-            .addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 201, GroupLayout.PREFERRED_SIZE)
-            .addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 201, GroupLayout.PREFERRED_SIZE)
-            .addGroup(groupLayout.createSequentialGroup()
-              .addGap(65)
-              .addComponent(lblScore, GroupLayout.PREFERRED_SIZE, 69, GroupLayout.PREFERRED_SIZE))
-            .addGroup(groupLayout.createSequentialGroup()
-              .addGap(84)
-              .addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 116, GroupLayout.PREFERRED_SIZE))
-            .addGroup(groupLayout.createSequentialGroup()
-              .addGap(65)
-              .addComponent(lblMovesLeft, GroupLayout.PREFERRED_SIZE, 135, GroupLayout.PREFERRED_SIZE))
-            .addGroup(groupLayout.createSequentialGroup()
-              .addGap(101)
-              .addComponent(label, GroupLayout.PREFERRED_SIZE, 99, GroupLayout.PREFERRED_SIZE)))
-          .addGap(10))
-    );
-    groupLayout.setVerticalGroup(
-      groupLayout.createParallelGroup(Alignment.LEADING)
-        .addGroup(groupLayout.createSequentialGroup()
-          .addGap(11)
-          .addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-            .addComponent(boardView, GroupLayout.PREFERRED_SIZE, 545, GroupLayout.PREFERRED_SIZE)
-            .addGroup(groupLayout.createSequentialGroup()
-              .addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
-                .addComponent(btnRestart, GroupLayout.PREFERRED_SIZE, 62, GroupLayout.PREFERRED_SIZE)
-                .addComponent(btnMainMenu, GroupLayout.PREFERRED_SIZE, 62, GroupLayout.PREFERRED_SIZE))
-              .addGap(11)
-              .addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 56, GroupLayout.PREFERRED_SIZE)
-              .addGap(31)
-              .addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
-              .addGap(21)
-              .addComponent(lblScore, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE)
-              .addGap(11)
-              .addComponent(lblNewLabel)
-              .addGap(110)
-              .addComponent(lblMovesLeft, GroupLayout.DEFAULT_SIZE, 27, Short.MAX_VALUE)
-              .addGap(10)
-              .addComponent(label, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
-              .addGap(79)))
-          .addGap(44))
-    );
-    setLayout(groupLayout);
-  }
+    }
 }
