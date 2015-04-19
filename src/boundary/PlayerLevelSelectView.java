87d2d6251bb6f84b37655f17dcfc400bea4c0275
Eli Skeggs <skeggse@gmail.com>
2015-04-19 15:57:27 -0400

    Fix formatting
    Signed-off-by: Eli Skeggs <esskeggs@wpi.edu>

--- src/boundary/PlayerLevelSelectView.java
+++ src/boundary/PlayerLevelSelectView.java
@@ -2,154 +2,150 @@ package boundary;
 
 import controller.PlayerLoadLevelCtrl;
 import controller.PlayerMainMenuCtrl;
-
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridLayout;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
-import javax.swing.BoxLayout;
-
 import model.PlayerModel;
 
-import java.awt.GridLayout;
-import java.awt.BorderLayout;
-
-
+/**
+ * The View for Selecting a Level
+ * @author Maurizio Vitale
+ */
 public class PlayerLevelSelectView extends JPanel {
-    /**
-     * The View for Selecting a Level
-     * @author Maurizio Vitale
-     */
-	  
+
   PlayerApplication app;
   PlayerModel model;
 
   public PlayerLevelSelectView(PlayerApplication app, PlayerModel model) {
 
-	this.app = app;
-    this.model = model;
-    
-    setMinimumSize(new Dimension(800,600));
-    setPreferredSize(new Dimension(800, 600));
-    setLayout(new GridLayout(0, 2, 0, 0));
-    
-    //Two SubPanels
-    
-    //panelLevel - contains level preview, name type and previous score, along with options for the level
-    
-    JPanel panelLevel = new JPanel();
-    panelLevel.setLayout(new BorderLayout(0, 0));
-    
-    //panelLevelPreview - where gameboard will be loaded
-    
-    JPanel panelLevelPreview = new JPanel();
-    panelLevel.add(panelLevelPreview, BorderLayout.CENTER);
-    
-    //panelLevelOptions - holds play level and reset score
-    
-    JPanel panelLevelOptions = new JPanel();
-    panelLevelOptions.setBorder(null);
-    panelLevel.add(panelLevelOptions, BorderLayout.SOUTH);
-    
-    //btnPLayLevel, explanatory
-    
-    JButton btnPlayLevel = new JButton("Play Level");
-    panelLevelOptions.add(btnPlayLevel);
-    btnPlayLevel.addActionListener(new PlayerLoadLevelCtrl(app, model));
-    
-    //debating heavily on this one, holds level info and navigation panel, 
-    //considering merging level info and gameboard into a single panel instead
-    
-    JPanel panelTopContent = new JPanel();
-    panelTopContent.setLayout(new BoxLayout(panelTopContent,BoxLayout.Y_AXIS));
-    panelLevel.add(panelTopContent, BorderLayout.NORTH);
-    
-    //panelNavigation holds whatever navigation purpose you need
-    
-    JPanel panelNavigation = new JPanel();
-    panelTopContent.add(panelNavigation);
-    
-    //btnMainMenu
-    
-    JButton btnMainMenu = new JButton("Main Menu");
-    btnMainMenu.setSize(btnMainMenu.getWidth(), 29);    
-    panelNavigation.add(btnMainMenu);
-    btnMainMenu.addActionListener(new PlayerMainMenuCtrl(app, model));
-    
-    JPanel panelLevelInfo = new JPanel();
-    
-    panelLevelInfo.setLayout(new GridLayout(0, 1, 0, 0));
-    
-    JLabel lblLevel = new JLabel("Level ");
-    panelLevelInfo.add(lblLevel);
-    lblLevel.setHorizontalAlignment(SwingConstants.CENTER);
-        
-    JLabel lblType = new JLabel("Type: ");
-    panelLevelInfo.add(lblType);
-            
-    JLabel lblScore = new JLabel("Score: ");
-    panelLevelInfo.add(lblScore);
-    
-    panelTopContent.add(panelLevelInfo);
-    
+  this.app = app;
+  this.model = model;
 
-    
-    this.add(panelLevel);
-    
+  setMinimumSize(new Dimension(800,600));
+  setPreferredSize(new Dimension(800, 600));
+  setLayout(new GridLayout(0, 2, 0, 0));
+
+  //Two SubPanels
+
+  //panelLevel - contains level preview, name type and previous score, along with options for the level
+
+  JPanel panelLevel = new JPanel();
+  panelLevel.setLayout(new BorderLayout(0, 0));
+
+  //panelLevelPreview - where gameboard will be loaded
+
+  JPanel panelLevelPreview = new JPanel();
+  panelLevel.add(panelLevelPreview, BorderLayout.CENTER);
+
+  //panelLevelOptions - holds play level and reset score
+
+  JPanel panelLevelOptions = new JPanel();
+  panelLevelOptions.setBorder(null);
+  panelLevel.add(panelLevelOptions, BorderLayout.SOUTH);
+
+  //btnPLayLevel, explanatory
+
+  JButton btnPlayLevel = new JButton("Play Level");
+  panelLevelOptions.add(btnPlayLevel);
+  btnPlayLevel.addActionListener(new PlayerLoadLevelCtrl(app, model));
+
+  //debating heavily on this one, holds level info and navigation panel,
+  //considering merging level info and gameboard into a single panel instead
+
+  JPanel panelTopContent = new JPanel();
+  panelTopContent.setLayout(new BoxLayout(panelTopContent,BoxLayout.Y_AXIS));
+  panelLevel.add(panelTopContent, BorderLayout.NORTH);
+
+  //panelNavigation holds whatever navigation purpose you need
+
+  JPanel panelNavigation = new JPanel();
+  panelTopContent.add(panelNavigation);
+
+  //btnMainMenu
+
+  JButton btnMainMenu = new JButton("Main Menu");
+  btnMainMenu.setSize(btnMainMenu.getWidth(), 29);
+  panelNavigation.add(btnMainMenu);
+  btnMainMenu.addActionListener(new PlayerMainMenuCtrl(app, model));
+
+  JPanel panelLevelInfo = new JPanel();
+
+  panelLevelInfo.setLayout(new GridLayout(0, 1, 0, 0));
+
+  JLabel lblLevel = new JLabel("Level ");
+  panelLevelInfo.add(lblLevel);
+  lblLevel.setHorizontalAlignment(SwingConstants.CENTER);
+
+  JLabel lblType = new JLabel("Type: ");
+  panelLevelInfo.add(lblType);
+
+  JLabel lblScore = new JLabel("Score: ");
+  panelLevelInfo.add(lblScore);
+
+  panelTopContent.add(panelLevelInfo);
+
+
+
+  this.add(panelLevel);
+
   //panelLevelGrid - Contains Buttons for each level that exists
-    JPanel panelLevelGrid = new JPanel();
-    panelLevelGrid.setLayout(new GridLayout(0, 4, 0, 0));
-    
-    //Buttons for Level Select Grid
-    JButton btnLevel1 = new JButton("Level 1");
-    panelLevelGrid.add(btnLevel1);
+  JPanel panelLevelGrid = new JPanel();
+  panelLevelGrid.setLayout(new GridLayout(0, 4, 0, 0));
 
-    JButton btnLevel2 = new JButton("Level 2");
-    panelLevelGrid.add(btnLevel2);
-    
-    JButton btnLevel3 = new JButton("Level 3");
-    panelLevelGrid.add(btnLevel3);
-    
-    JButton btnLevel4 = new JButton("Level 4");
-    panelLevelGrid.add(btnLevel4);
-    
-    JButton btnLevel5 = new JButton("Level 5");
-    panelLevelGrid.add(btnLevel5);
-    
-    JButton btnLevel6 = new JButton("Level 6");
-    panelLevelGrid.add(btnLevel6);
-    
-    JButton btnLevel7 = new JButton("Level 7");
-    panelLevelGrid.add(btnLevel7);
-    
-    JButton btnLevel8 = new JButton("Level 8");
-    panelLevelGrid.add(btnLevel8);
-    
-    JButton btnLevel9 = new JButton("Level 9");
-    panelLevelGrid.add(btnLevel9);
-    
-    JButton btnLevel10 = new JButton("Level 10");
-    panelLevelGrid.add(btnLevel10);
-    
-    JButton btnLevel11 = new JButton("Level 11");
-    panelLevelGrid.add(btnLevel11);
-    
-    JButton btnLevel12 = new JButton("Level 12");
-    panelLevelGrid.add(btnLevel12);
-    
-    JButton btnLevel13 = new JButton("Level 13");
-    panelLevelGrid.add(btnLevel13);
-    
-    JButton btnLevel14 = new JButton("Level 14");
-    panelLevelGrid.add(btnLevel14);
-    
-    JButton btnLevel15 = new JButton("Level 15");
-    panelLevelGrid.add(btnLevel15);
-    
-    JButton btnLevel16 = new JButton("Level 16");
-    panelLevelGrid.add(btnLevel16);
-    
-    this.add(panelLevelGrid);
+  //Buttons for Level Select Grid
+  JButton btnLevel1 = new JButton("Level 1");
+  panelLevelGrid.add(btnLevel1);
+
+  JButton btnLevel2 = new JButton("Level 2");
+  panelLevelGrid.add(btnLevel2);
+
+  JButton btnLevel3 = new JButton("Level 3");
+  panelLevelGrid.add(btnLevel3);
+
+  JButton btnLevel4 = new JButton("Level 4");
+  panelLevelGrid.add(btnLevel4);
+
+  JButton btnLevel5 = new JButton("Level 5");
+  panelLevelGrid.add(btnLevel5);
+
+  JButton btnLevel6 = new JButton("Level 6");
+  panelLevelGrid.add(btnLevel6);
+
+  JButton btnLevel7 = new JButton("Level 7");
+  panelLevelGrid.add(btnLevel7);
+
+  JButton btnLevel8 = new JButton("Level 8");
+  panelLevelGrid.add(btnLevel8);
+
+  JButton btnLevel9 = new JButton("Level 9");
+  panelLevelGrid.add(btnLevel9);
+
+  JButton btnLevel10 = new JButton("Level 10");
+  panelLevelGrid.add(btnLevel10);
+
+  JButton btnLevel11 = new JButton("Level 11");
+  panelLevelGrid.add(btnLevel11);
+
+  JButton btnLevel12 = new JButton("Level 12");
+  panelLevelGrid.add(btnLevel12);
+
+  JButton btnLevel13 = new JButton("Level 13");
+  panelLevelGrid.add(btnLevel13);
+
+  JButton btnLevel14 = new JButton("Level 14");
+  panelLevelGrid.add(btnLevel14);
+
+  JButton btnLevel15 = new JButton("Level 15");
+  panelLevelGrid.add(btnLevel15);
+
+  JButton btnLevel16 = new JButton("Level 16");
+  panelLevelGrid.add(btnLevel16);
+
+  this.add(panelLevelGrid);
   }
 }
--- src/boundary/PlayerMainMenuView.java
+++ src/boundary/PlayerMainMenuView.java
@@ -2,17 +2,17 @@ package boundary;
 
 import controller.ExitCtrl;
 import controller.PlayerLoadLevelSelectCtrl;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
-import javax.swing.BoxLayout;
 import model.PlayerModel;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Component;
 
 /**
  * PlayerMainMenuView for the player application
@@ -20,49 +20,49 @@ import java.awt.Component;
  * @author Bailey Sheridan
  */
 public class PlayerMainMenuView extends JPanel {
-	PlayerModel model;
-	PlayerApplication app;
+  PlayerModel model;
+  PlayerApplication app;
 
-	public PlayerMainMenuView(PlayerApplication app, PlayerModel model) {
-		this.app = app;
-		this.model = model;
+  public PlayerMainMenuView(PlayerApplication app, PlayerModel model) {
+    this.app = app;
+    this.model = model;
 
-		setMinimumSize(new Dimension(600,480));
-		setPreferredSize(new Dimension(600, 480));
+    setMinimumSize(new Dimension(600,480));
+    setPreferredSize(new Dimension(600, 480));
 
-		GridBagLayout gridBagLayout = new GridBagLayout();
+    GridBagLayout gridBagLayout = new GridBagLayout();
 
-		setLayout(gridBagLayout);
+    setLayout(gridBagLayout);
 
-		JPanel panelContent = new JPanel();
-		panelContent.setBorder(null);
-		panelContent.setSize(300,500);
+    JPanel panelContent = new JPanel();
+    panelContent.setBorder(null);
+    panelContent.setSize(300,500);
 
-		add(panelContent);
-		panelContent.setLayout(new BoxLayout(panelContent, BoxLayout.Y_AXIS));
+    add(panelContent);
+    panelContent.setLayout(new BoxLayout(panelContent, BoxLayout.Y_AXIS));
 
-		JLabel lblSixesWild = new JLabel("Sixes Wild");
-		panelContent.add(lblSixesWild);
-		lblSixesWild.setFont(new Font("Matura MT Script Capitals", Font.PLAIN, 26));
-		lblSixesWild.setAlignmentX(Component.CENTER_ALIGNMENT);
-		lblSixesWild.setAlignmentY(Component.CENTER_ALIGNMENT);
+    JLabel lblSixesWild = new JLabel("Sixes Wild");
+    panelContent.add(lblSixesWild);
+    lblSixesWild.setFont(new Font("Matura MT Script Capitals", Font.PLAIN, 26));
+    lblSixesWild.setAlignmentX(Component.CENTER_ALIGNMENT);
+    lblSixesWild.setAlignmentY(Component.CENTER_ALIGNMENT);
 
-		JButton btnLevelSelect = new JButton("Level Select");
-		panelContent.add(btnLevelSelect);
-		btnLevelSelect.addActionListener(new PlayerLoadLevelSelectCtrl(app, model));
-		btnLevelSelect.setAlignmentX(Component.CENTER_ALIGNMENT);
-		btnLevelSelect.setAlignmentY(Component.CENTER_ALIGNMENT);
+    JButton btnLevelSelect = new JButton("Level Select");
+    panelContent.add(btnLevelSelect);
+    btnLevelSelect.addActionListener(new PlayerLoadLevelSelectCtrl(app, model));
+    btnLevelSelect.setAlignmentX(Component.CENTER_ALIGNMENT);
+    btnLevelSelect.setAlignmentY(Component.CENTER_ALIGNMENT);
 
 
-		JButton btnNewButton = new JButton("Achievements");
-		panelContent.add(btnNewButton);
-		btnNewButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-		btnNewButton.setAlignmentY(Component.CENTER_ALIGNMENT);
+    JButton btnNewButton = new JButton("Achievements");
+    panelContent.add(btnNewButton);
+    btnNewButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+    btnNewButton.setAlignmentY(Component.CENTER_ALIGNMENT);
 
-		JButton btnExit = new JButton("Exit");
-		panelContent.add(btnExit);
-		btnExit.addActionListener(new ExitCtrl());
-		btnExit.setAlignmentX(Component.CENTER_ALIGNMENT);
-		btnExit.setAlignmentY(Component.CENTER_ALIGNMENT);
-	}
+    JButton btnExit = new JButton("Exit");
+    panelContent.add(btnExit);
+    btnExit.addActionListener(new ExitCtrl());
+    btnExit.setAlignmentX(Component.CENTER_ALIGNMENT);
+    btnExit.setAlignmentY(Component.CENTER_ALIGNMENT);
+  }
 }
