4538e592ac506a25807858854bf6be02d15d8e4d
Eli Skeggs <skeggse@gmail.com>
2015-04-19 18:45:24 -0400

    Fix PlayerRestartLevelCtrl
    Signed-off-by: Eli Skeggs <esskeggs@wpi.edu>

--- src/boundary/PlayerMainMenuView.java
+++ src/boundary/PlayerMainMenuView.java
@@ -7,14 +7,14 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import model.PlayerModel;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 /**
  * PlayerMainMenuView for the player application
@@ -55,12 +55,7 @@ public class PlayerMainMenuView extends JPanel {
     btnLevelSelect.setAlignmentX(Component.CENTER_ALIGNMENT);
     btnLevelSelect.setAlignmentY(Component.CENTER_ALIGNMENT);
 
-
     JButton btnNewButton = new JButton("Instructions");
-    btnNewButton.addActionListener(new ActionListener() {
-    	public void actionPerformed(ActionEvent arg0) {
-    	}
-    });
     panelContent.add(btnNewButton);
     btnNewButton.setAlignmentX(Component.CENTER_ALIGNMENT);
     btnNewButton.setAlignmentY(Component.CENTER_ALIGNMENT);
--- src/controller/PlayerRestartLevelCtrl.java
+++ src/controller/PlayerRestartLevelCtrl.java
@@ -1,22 +1,26 @@
 package controller;
 
 import boundary.PlayerApplication;
+import boundary.PlayerBoardView;
+import boundary.PlayerLevelView;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import model.PlayerModel;
 
 public class PlayerRestartLevelCtrl implements ActionListener{
-	
-	 PlayerApplication app;
-	  PlayerModel model;
+  PlayerApplication app;
+  PlayerModel model;
 
-	  public PlayerRestartLevelCtrl(PlayerApplication app, PlayerModel model) {
-	    this.app = app;
-	    this.model = model;
-	  }
+  public PlayerRestartLevelCtrl(PlayerApplication app, PlayerModel model) {
+    this.app = app;
+    this.model = model;
+  }
 
-	  public void actionPerformed(ActionEvent event) {
-		  model.level.discardBoard();
-		  model.level.realizeBoard();
-	  }
+  public void actionPerformed(ActionEvent event) {
+    model.level.discardBoard();
+    model.level.realizeBoard();
+
+    PlayerBoardView boardView = ((PlayerLevelView) app.getView()).boardView;
+    boardView.repaint();
+  }
 }
