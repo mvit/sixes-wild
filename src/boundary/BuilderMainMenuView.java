c9273ad02309884e24f2aaba2f9df01ac6d8c8b8
Maurizio Vitale <mauriziovit@gmail.com>
2015-04-19 22:06:04 -0400

    Add whitespace to BuilderMainMenu

--- src/boundary/BuilderMainMenuView.java
+++ src/boundary/BuilderMainMenuView.java
@@ -26,16 +26,22 @@ public class BuilderMainMenuView extends JPanel {
     
     setMinimumSize(new Dimension(600,480));
     setPreferredSize(new Dimension(600, 480));
+    
     //Make Layout GridBagLayout
+    
     GridBagLayout gridBagLayout = new GridBagLayout();
     setLayout(gridBagLayout);
+    
     //Add ContentPanel in center
+    
     JPanel panelContent = new JPanel();
     panelContent.setBorder(null);
     panelContent.setSize(300,500);
     panelContent.setLayout(new BoxLayout(panelContent, BoxLayout.Y_AXIS));
     add(panelContent);
+    
     //Add Labels and Buttons to center ContentPanel
+    
     JLabel lblBuilder = new JLabel("\u262D Builder");
     lblBuilder.setAlignmentX(Component.CENTER_ALIGNMENT);
     lblBuilder.setAlignmentY(Component.CENTER_ALIGNMENT);
