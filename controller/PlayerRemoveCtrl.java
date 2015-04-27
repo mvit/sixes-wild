c1c72c328a2b380fa6a216234507d8ed7e58ee52
nchaput <nchaput@wpi.edu>
2015-04-26 22:28:01 -0400

    Fix logic error in PlayerRemoveCtrl

--- controller/PlayerRemoveCtrl.java
+++ controller/PlayerRemoveCtrl.java
@@ -17,10 +17,11 @@ public class PlayerRemoveCtrl {
   public void mouseClicked(Point point) {
 	    //TODO: generic implementation
 	  PlayerVariationCtrl pVar = model.variation.createCtrl(app, model);
-	  if (model.variation != Variation.RELEASE && model.level.currentBoard.grid[point.x][point.y].tile.number == 6)
+	  if (model.variation != Variation.RELEASE && model.level.currentBoard.grid[point.x][point.y].tile.number != 6)
 	  {
 		  //GO INTO PLAYERMODEL AND REPLACE PLAYERVARIATIONCTRL WITH VARIATION
 		  //TODO: This is suboptimal, decide whether to change PlayerVariationCtrl.specialMove() to individual methods for each special move
+		  //TODO: Create new move, resolve the move
 	  }
 	  pVar.specialMove();
   }
