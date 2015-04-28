23bba2577a14137b28807f020a7aec69ca1ec9e6
Eli Skeggs <skeggse@gmail.com>
2015-04-28 10:44:05 -0400

    Format, cleanup slider code
    Signed-off-by: Eli Skeggs <esskeggs@wpi.edu>

--- boundary/BuilderLevelEditorView.java
+++ boundary/BuilderLevelEditorView.java
@@ -27,6 +27,7 @@ import javax.swing.JSlider;
 
 import model.BuilderModel;
 import model.CellType;
+import model.Rules;
 import model.Variation;
 
 /**
@@ -48,14 +49,15 @@ public class BuilderLevelEditorView extends JPanel {
 
     setMinimumSize(new Dimension(600,480));
     setPreferredSize(new Dimension(600, 480));
-    setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+    setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-    // panelControls contain all top control buttons
+    // contains all top control buttons
 
     JPanel panelTopControls = new JPanel();
     panelTopControls.setAlignmentX(0);
     panelTopControls.setAlignmentY(0);
-    panelTopControls.setLayout(new BoxLayout(panelTopControls, BoxLayout.X_AXIS));
+    panelTopControls.setLayout(new BoxLayout(panelTopControls,
+      BoxLayout.X_AXIS));
     add(panelTopControls);
 
     JButton btnNew = new JButton("New");
@@ -74,14 +76,14 @@ public class BuilderLevelEditorView extends JPanel {
     btnClose.addActionListener(new BuilderMainMenuCtrl(app, model));
     panelTopControls.add(btnClose);
 
-    //panelGame contains all Game related options
+    // contains all game-related options
 
     JPanel panelGame = new JPanel();
     panelGame.setAlignmentX(0);
     panelGame.setLayout(new BoxLayout(panelGame, BoxLayout.X_AXIS));
     add(panelGame);
 
-    //panelControls contains controls related to the board
+    // contains controls related to the board
 
     JPanel panelControls = new JPanel();
     panelControls.setLayout(new BoxLayout(panelControls, BoxLayout.Y_AXIS));
@@ -115,14 +117,14 @@ public class BuilderLevelEditorView extends JPanel {
       CellType.BUCKET));
     panelControls.add(btnMakeSlot);
 
-    //panelBoard contains panelType and panelGrid
+    // contains the variation radio buttons and the boardView
 
     JPanel panelBoard = new JPanel();
     panelBoard.setAlignmentY(0);
     panelGame.add(panelBoard);
     panelBoard.setLayout(new BorderLayout());
 
-    //panelType contains mutually exclusive radio buttons
+    // contains variation radio buttons
 
     JPanel panelType = new JPanel();
     panelType.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
@@ -136,7 +138,7 @@ public class BuilderLevelEditorView extends JPanel {
       panelType.add(button);
     }
 
-    //Button Group contains radio Buttons to make them mutually exclusive
+    // contains radio buttons for mutual exclusivity
 
     variationGroup = new ButtonGroup();
 
@@ -146,123 +148,41 @@ public class BuilderLevelEditorView extends JPanel {
 
     variationButtons.get(model.level.rules.variation).setSelected(true);
 
-    //boardView contains the board
+    // contains the board
 
     boardView = new BuilderBoardView(app, model);
     boardView.setAlignmentY(0);
     panelBoard.add(boardView);
 
-    //panelSliders contain probability Sliders for number generation
+    // contains the probability sliders for number generation
+
     JPanel panelSliders = new JPanel();
     panelGame.add(panelSliders);
     panelSliders.setAlignmentY(0);
     panelSliders.setLayout(new BoxLayout(panelSliders, BoxLayout.Y_AXIS));
 
-    //Code for each slider
-    int x = 20;//Major Tick Spacing
-    int y = 10;//Minor Tick Spacing
+    int x = 20; // major tick spacing
+    int y = 10; // minor tick spacing
 
-    //Slider 1
-    //Make the panel
-    JPanel panelSlider1 = new JPanel();
-    panelSlider1.setLayout(new BoxLayout(panelSlider1, BoxLayout.X_AXIS));
-    panelSliders.add(panelSlider1);
-    //Make the Label
-    JLabel lblNum1 = new JLabel("1");
-    panelSlider1.add(lblNum1);
-    //Make the Slider
-    JSlider slider1 = new JSlider();
-    slider1.setPaintLabels(true);
-    slider1.setMajorTickSpacing(x);
-    slider1.setMinorTickSpacing(y);
-    slider1.setSnapToTicks(true);
-    slider1.setPaintTicks(true);
-    panelSlider1.add(slider1);
+    // make the slider panels, styling specific to a given panel can be put in
+    // an array, which would then be used in the loop
+    for (int i = 0; i < Rules.maxNumber; i++) {
+      JPanel panel = new JPanel();
+      panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
 
-    //Slider 2
-    //Make the panel
-    JPanel panelSlider2 = new JPanel();
-    panelSlider2.setLayout(new BoxLayout(panelSlider2, BoxLayout.X_AXIS));
-    panelSliders.add(panelSlider2);
-    //Make the Label
-    JLabel lblNum2 = new JLabel("2");
-    panelSlider2.add(lblNum2);
-    //Make the Slider
-    JSlider slider2 = new JSlider();
-    slider2.setPaintLabels(true);
-    slider2.setMajorTickSpacing(x);
-    slider2.setMinorTickSpacing(y);
-    slider2.setSnapToTicks(true);
-    slider2.setPaintTicks(true);
-    panelSlider2.add(slider2);
+      JLabel label = new JLabel("" + (i + 1));
+      panel.add(label);
 
-    //Slider 3
-    //Make the panel
-    JPanel panelSlider3 = new JPanel();
-    panelSlider3.setLayout(new BoxLayout(panelSlider3, BoxLayout.X_AXIS));
-    panelSliders.add(panelSlider3);
-    //Make the Label
-    JLabel lblNum3 = new JLabel("3");
-    panelSlider3.add(lblNum3);
-    //Make the Slider
-    JSlider slider3 = new JSlider();
-    slider3.setPaintLabels(true);
-    slider3.setMajorTickSpacing(x);
-    slider3.setMinorTickSpacing(y);
-    slider3.setSnapToTicks(true);
-    slider3.setPaintTicks(true);
-    panelSlider3.add(slider3);
+      JSlider slider = new JSlider();
+      slider.setPaintLabels(true);
+      slider.setMajorTickSpacing(20);
+      slider.setMinorTickSpacing(10);
+      slider.setSnapToTicks(true);
+      slider.setPaintTicks(true);
+      panel.add(slider);
 
-    //Slider 4
-    //Make the panel
-    JPanel panelSlider4 = new JPanel();
-    panelSlider4.setLayout(new BoxLayout(panelSlider4, BoxLayout.X_AXIS));
-    panelSliders.add(panelSlider4);
-    //Make the Label
-    JLabel lblNum4 = new JLabel("4");
-    panelSlider4.add(lblNum4);
-    //Make the Slider
-    JSlider slider4 = new JSlider();
-    slider4.setPaintLabels(true);
-    slider4.setMajorTickSpacing(x);
-    slider4.setMinorTickSpacing(y);
-    slider4.setSnapToTicks(true);
-    slider4.setPaintTicks(true);
-    panelSlider4.add(slider4);
-
-    //Slider 5
-    //Make the panel
-    JPanel panelSlider5 = new JPanel();
-    panelSlider5.setLayout(new BoxLayout(panelSlider5, BoxLayout.X_AXIS));
-    panelSliders.add(panelSlider5);
-    //Make the Label
-    JLabel lblNum5 = new JLabel("5");
-    panelSlider5.add(lblNum5);
-    //Make the Slider
-    JSlider slider5 = new JSlider();
-    slider5.setPaintLabels(true);
-    slider5.setMajorTickSpacing(x);
-    slider5.setMinorTickSpacing(y);
-    slider5.setSnapToTicks(true);
-    slider5.setPaintTicks(true);
-    panelSlider5.add(slider5);
-
-    //Slider 6
-    //Make the panel
-    JPanel panelSlider6 = new JPanel();
-    panelSlider6.setLayout(new BoxLayout(panelSlider6, BoxLayout.X_AXIS));
-    panelSliders.add(panelSlider6);
-    //Make the Label
-    JLabel lblNum6 = new JLabel("6");
-    panelSlider6.add(lblNum6);
-    //Make the Slider
-    JSlider slider6 = new JSlider();
-    slider6.setPaintLabels(true);
-    slider6.setMajorTickSpacing(x);
-    slider6.setMinorTickSpacing(y);
-    slider6.setSnapToTicks(true);
-    slider6.setPaintTicks(true);
-    panelSlider6.add(slider6);
+      panelSliders.add(panel);
+    }
   }
 
   public void updateView() {
