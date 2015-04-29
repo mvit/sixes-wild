mvit/sixes-wild

commit 6deaab2f7c002c0faa6094a494bdcedb45e32067

Implement undo and redo
Signed-off-by: Eli Skeggs <esskeggs@wpi.edu>

model/BuilderModel.java
@@ -23,4 +23,35 @@ public class BuilderModel {
    * @constructor
    */
   public BuilderModel() {}
+
+  /**
+   * Clear the temporary state.
+   */
+  public void clearTempState() {
+    currentType = null;
+    currentUseNumber = false;
+    currentNumber = 0;
+    currentMultiplier = 1;
+  }
+
+  public void clearHistory() {
+    history.removeAll();
+    history.trimToSize();
+  }
+
+  /**
+   * Adds a snapshot of the current state to the history.
+   */
+  public void takeSnapshot() {
+    if (redoIndex < history.size() - 1) {
+      history.subList(redoIndex + 1, history.size()).clear();
+      history.trimToSize();
+    }
+
+    if (history.size() > 0) {
+      redoIndex++;
+    }
+
+    history.add(new Change(new Level(level)));
+  }
 }

controller/BuilderBoardMouseCtrl.java
@@ -66,9 +66,10 @@ public class BuilderBoardMouseCtrl extends MouseAdapter {
 
   @Override
   public void mouseReleased(MouseEvent event) {
-    if (event.getButton() == MouseEvent.BUTTON1) {
+    if (dragging && event.getButton() == MouseEvent.BUTTON1) {
       applyCell(event);
       dragging = false;
+      model.takeSnapshot();
     }
   }
 

controller/BuilderMainMenuCtrl.java
@@ -27,6 +27,7 @@ public class BuilderMainMenuCtrl implements ActionListener {
    * Switch to the main menu.
    */
   public void loadMainScreen() {
+    model.clearHistory();
     app.setView(new BuilderMainMenuView(app, model));
   }
 

controller/BuilderNewLevelCtrl.java
@@ -28,8 +28,9 @@ public class BuilderNewLevelCtrl implements ActionListener {
    * Load the level select view.
    */
   public void loadEditorView() {
-    // model.level = new Level(new DataInputStream(new FileInputStream(filename)));
     model.level = new Level();
+    model.clearHistory();
+    model.takeSnapshot();
     app.setView(new BuilderLevelEditorView(app, model));
   }
 

controller/BuilderOpenLevelCtrl.java
@@ -51,6 +51,9 @@ public class BuilderOpenLevelCtrl implements ActionListener {
         return;
       }
 
+      model.clearHistory();
+      model.takeSnapshot();
+
       app.setView(new BuilderLevelEditorView(app, model));
     }
   }

controller/BuilderRedoCtrl.java
@@ -20,10 +20,14 @@ public class BuilderRedoCtrl implements ActionListener {
 
   @Override
   public void actionPerformed(ActionEvent e) {
-    if (model.redoIndex > 0) {
-      model.level = model.history.get(--model.redoIndex).snapshot;
+    if (model.redoIndex < model.history.size() - 1) {
+      model.level = model.history.get(++model.redoIndex).snapshot;
+      model.clearTempState();
 
-      ((BuilderLevelEditorView) app.getView()).updateView();
+      BuilderLevelEditorView view = (BuilderLevelEditorView) app.getView();
+      view.updateView();
+      view.boardView.repaint();
+      view.revalidate();
     }
   }
 }

controller/BuilderSetVariationCtrl.java
@@ -28,5 +28,6 @@ public class BuilderSetVariationCtrl implements ActionListener {
   @Override
   public void actionPerformed(ActionEvent event) {
     model.level.rules.variation = variation;
+    model.takeSnapshot();
   }
 }

controller/BuilderUndoCtrl.java
@@ -19,12 +19,15 @@ public class BuilderUndoCtrl implements ActionListener {
   }
 
   @Override
-  public void actionPerformed(ActionEvent e) {
+  public void actionPerformed(ActionEvent event) {
     if (model.redoIndex > 0) {
-      model.redoIndex--;
-      model.level = model.history.get(model.redoIndex).snapshot;
+      model.level = model.history.get(--model.redoIndex).snapshot;
+      model.clearTempState();
 
-      ((BuilderLevelEditorView) app.getView()).updateView();
+      BuilderLevelEditorView view = (BuilderLevelEditorView) app.getView();
+      view.updateView();
+      view.boardView.repaint();
+      view.revalidate();
     }
   }
 }

commit e9bdb6d1fd3d64ec68f654f0acf3bf7848c30be2

Fix compiler error introduced in 6deaab2
Signed-off-by: Eli Skeggs <esskeggs@wpi.edu>

model/BuilderModel.java
@@ -35,7 +35,7 @@ public class BuilderModel {
   }
 
   public void clearHistory() {
-    history.removeAll();
+    history.clear();
     history.trimToSize();
   }
 