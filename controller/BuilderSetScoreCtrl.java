d53c188e686329ad4053f6cd46378ce3598e64a2
cmunsal <cmunsal@wpi.edu>
2015-04-29 12:48:23 -0400

    Add Score Controler

---
+++ controller/BuilderSetScoreCtrl.java
@@ -0,0 +1,36 @@
+package controller;
+
+import boundary.BuilderApplication;
+import boundary.BuilderLevelEditorView;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JTextField;
+
+import model.BuilderModel;
+
+/**
+ * @author Cem Unsal
+ */
+public class BuilderSetScoreCtrl implements ActionListener {
+  BuilderApplication app;
+  BuilderModel model;
+  int tier;
+
+  public BuilderSetScoreCtrl(BuilderApplication app, BuilderModel model, int tier) {
+    this.app = app;
+    this.model = model;
+    this.tier = tier;
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+	  String entry = ((JTextField) e.getSource()).getText();
+	  int num = Integer.parseInt(entry);
+	  
+	  model.level.rules.scoreThresholds[tier]=num;
+	  model.takeSnapshot();
+	  
+  }
+}
