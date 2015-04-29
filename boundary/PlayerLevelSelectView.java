2a2cd33c6954b29407b7542ba7b697a5fc26b483
Eli Skeggs <skeggse@gmail.com>
2015-04-28 23:46:05 -0400

    Fix indentation, improve level select init
    Signed-off-by: Eli Skeggs <esskeggs@wpi.edu>

--- boundary/PlayerLevelSelectView.java
+++ boundary/PlayerLevelSelectView.java
@@ -14,109 +14,130 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
+import model.Level;
 import model.PlayerModel;
 
 /**
  * The View for Selecting a Level.
  *
- * @author Maurizio Vitale
+ * @author Eli Skeggs, and Maurizio Vitale
  */
 public class PlayerLevelSelectView extends JPanel {
-
   PlayerApplication app;
   PlayerModel model;
 
   public PlayerLevelSelectView(PlayerApplication app, PlayerModel model) {
+    this.app = app;
+    this.model = model;
 
-  this.app = app;
-  this.model = model;
+    setMinimumSize(new Dimension(800,600));
+    setPreferredSize(new Dimension(800, 600));
+    setLayout(new GridLayout(0, 2, 0, 0));
 
-  setMinimumSize(new Dimension(800,600));
-  setPreferredSize(new Dimension(800, 600));
-  setLayout(new GridLayout(0, 2, 0, 0));
+    //Two SubPanels
 
-  //Two SubPanels
+    //panelLevel - contains level preview, name type and previous score, along with options for the level
 
-  //panelLevel - contains level preview, name type and previous score, along with options for the level
+    JPanel panelLevel = new JPanel();
+    panelLevel.setLayout(new BorderLayout(0, 0));
 
-  JPanel panelLevel = new JPanel();
-  panelLevel.setLayout(new BorderLayout(0, 0));
+    //panelLevelPreview - where gameboard will be loaded
 
-  //panelLevelPreview - where gameboard will be loaded
+    JPanel panelLevelPreview = new JPanel();
+    panelLevel.add(panelLevelPreview, BorderLayout.CENTER);
 
-  JPanel panelLevelPreview = new JPanel();
-  panelLevel.add(panelLevelPreview, BorderLayout.CENTER);
+    //panelLevelOptions - holds play level and reset score
 
-  //panelLevelOptions - holds play level and reset score
+    JPanel panelLevelOptions = new JPanel();
+    panelLevelOptions.setBorder(null);
+    panelLevel.add(panelLevelOptions, BorderLayout.SOUTH);
 
-  JPanel panelLevelOptions = new JPanel();
-  panelLevelOptions.setBorder(null);
-  panelLevel.add(panelLevelOptions, BorderLayout.SOUTH);
+    //btnPLayLevel, explanatory
 
-  //btnPLayLevel, explanatory
+    JButton btnPlayLevel = new JButton("Play Level");
+    panelLevelOptions.add(btnPlayLevel);
+    btnPlayLevel.addActionListener(new PlayerLoadLevelCtrl(app, model));
 
-  JButton btnPlayLevel = new JButton("Play Level");
-  panelLevelOptions.add(btnPlayLevel);
-  btnPlayLevel.addActionListener(new PlayerLoadLevelCtrl(app, model));
+    //debating heavily on this one, holds level info and navigation panel,
+    //considering merging level info and gameboard into a single panel instead
 
-  //debating heavily on this one, holds level info and navigation panel,
-  //considering merging level info and gameboard into a single panel instead
+    JPanel panelTopContent = new JPanel();
+    panelTopContent.setLayout(new BoxLayout(panelTopContent,BoxLayout.Y_AXIS));
+    panelLevel.add(panelTopContent, BorderLayout.NORTH);
 
-  JPanel panelTopContent = new JPanel();
-  panelTopContent.setLayout(new BoxLayout(panelTopContent,BoxLayout.Y_AXIS));
-  panelLevel.add(panelTopContent, BorderLayout.NORTH);
+    //panelNavigation holds whatever navigation purpose you need
 
-  //panelNavigation holds whatever navigation purpose you need
+    JPanel panelNavigation = new JPanel();
+    panelTopContent.add(panelNavigation);
 
-  JPanel panelNavigation = new JPanel();
-  panelTopContent.add(panelNavigation);
+    //btnMainMenu
 
-  //btnMainMenu
+    JButton btnMainMenu = new JButton("Main Menu");
+    btnMainMenu.setSize(btnMainMenu.getWidth(), 29);
+    panelNavigation.add(btnMainMenu);
+    btnMainMenu.addActionListener(new PlayerMainMenuCtrl(app, model));
 
-  JButton btnMainMenu = new JButton("Main Menu");
-  btnMainMenu.setSize(btnMainMenu.getWidth(), 29);
-  panelNavigation.add(btnMainMenu);
-  btnMainMenu.addActionListener(new PlayerMainMenuCtrl(app, model));
+    JPanel panelLevelInfo = new JPanel();
 
-  JPanel panelLevelInfo = new JPanel();
+    panelLevelInfo.setLayout(new GridLayout(0, 1, 0, 0));
 
-  panelLevelInfo.setLayout(new GridLayout(0, 1, 0, 0));
+    JLabel lblLevel = new JLabel("Level ");
+    panelLevelInfo.add(lblLevel);
+    lblLevel.setHorizontalAlignment(SwingConstants.CENTER);
 
-  JLabel lblLevel = new JLabel("Level ");
-  panelLevelInfo.add(lblLevel);
-  lblLevel.setHorizontalAlignment(SwingConstants.CENTER);
+    JLabel lblType = new JLabel("Type: ");
+    panelLevelInfo.add(lblType);
 
-  JLabel lblType = new JLabel("Type: ");
-  panelLevelInfo.add(lblType);
+    JLabel lblScore = new JLabel("Score: ");
+    panelLevelInfo.add(lblScore);
 
-  JLabel lblScore = new JLabel("Score: ");
-  panelLevelInfo.add(lblScore);
+    panelTopContent.add(panelLevelInfo);
 
-  panelTopContent.add(panelLevelInfo);
+    add(panelLevel);
 
-
-
-  this.add(panelLevel);
-
-  this.add(makeLevelGrid());
+    add(makeLevelGrid());
   }
-  
-  private JPanel makeLevelGrid() {
-	  JPanel content = new JPanel();
-	  content.setLayout(new GridLayout(4,4,0,0));
-	  int i = 1;
-	  File dir = new File("resource/levels/");
-	  File[] list = dir.listFiles();
-	  if (list != null){
-		  for(File file: list) {
-			 if (!file.getName().equals("README.md")) {
-			  JPanel panelLevel = new JPanel();
-			  JButton btnLevel = new JButton(file.getName());
-			  panelLevel.add(btnLevel);
-			  content.add(panelLevel);
-			 }
-		  }
-	  }
-	  return content;
+
+  /**
+   * Make a grid of buttons for level selection by reading the levels resource
+   * directory
+   *
+   * @return The new JPanel containing the level select buttons.
+   */
+  protected JPanel makeLevelGrid() {
+    File dir = new File("resource/levels/");
+    File[] list = dir.listFiles();
+
+    JPanel content = new JPanel();
+
+    if (list == null) {
+      // TODO: dialog?
+      return content;
+    }
+
+    int width = 4, height = (int) Math.ceil((double) list.length / width);
+    content.setLayout(new GridLayout(height, width, 0, 0));
+
+    for (File file : list) {
+      // ensure the filename is numeric
+      String name = file.getName();
+      int levelNumber;
+      try {
+        levelNumber = Integer.parseInt(name, 10);
+      } catch (NumberFormatException err) {
+        continue;
+      }
+
+      // exclude other files by checking the header
+      if (levelNumber > 0 && Level.checkHeader(file)) {
+        JPanel panelLevel = new JPanel();
+        // TODO: when we add the action listeners to these buttons, they should
+        // reference (levelNumber - 1)
+        JButton selectButton = new JButton(name);
+        panelLevel.add(selectButton);
+        content.add(panelLevel);
+      }
+    }
+    return content;
   }
 }
