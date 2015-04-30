491153ea95258ce936226c2ae0d67392527056f0
nchaput <nchaput@wpi.edu>
2015-04-29 20:19:08 -0400

    Tweak tile falling delay

--- controller/PlayerUpdateBoardCtrl.java
+++ controller/PlayerUpdateBoardCtrl.java
@@ -36,28 +36,28 @@ public class PlayerUpdateBoardCtrl {
 						  && model.level.currentBoard.grid[y][x].tile == null) {
 					  model.level.currentBoard.grid[y][x].tile = model.level.currentBoard.takeTileAbove(new Point(x,y));
 					  hasEmpty = true;
-					    /*try {
-						       Thread.sleep(30);
-								 app.getView().paintImmediately(50, 50, 500, 500);
+					    try {
+						       Thread.sleep(10);
+								 app.getView().paintImmediately(0, 0, 500, 500);
 						    } catch (InterruptedException ie) {
-						    }*/
+						    }
 				  }
 				  if (model.level.currentBoard.grid[y][x].type == CellType.BUCKET
 						  && model.level.currentBoard.getTileAbove(new Point(x,y)).number == 5
 						  && model.level.currentBoard.grid[y][x].tile == null) {
 					  model.level.currentBoard.grid[y][x].tile = model.level.currentBoard.takeTileAbove(new Point(x,y));
 					  hasEmpty = true;
-					    /*try {
-						       Thread.sleep(30);
-								 app.getView().paintImmediately(50, 50, 500, 500);
+					    try {
+						       Thread.sleep(10);
+								 app.getView().paintImmediately(0, 0, 500, 500);
 						    } catch (InterruptedException ie) {
-						    }*/
+						    }
 				  }
 			  }
 			  if (hasEmpty) {
 				  try {
-				       Thread.sleep(60);
-						 app.getView().paintImmediately(0, 0, 500, 500);
+				       Thread.sleep(70);
+						// app.getView().paintImmediately(0, 0, 500, 500);
 				    } catch (InterruptedException ie) {
 				    }
 			  }
