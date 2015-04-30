3facddbe32a8abd4fcb42c5c07bbc0a3e746e81b
Bailey <bsheridan@wpi.edu>
2015-04-30 09:40:59 -0400

    Add appropriate message to the end level message

--- controller/PlayerEndLevelCtrl.java
+++ controller/PlayerEndLevelCtrl.java
@@ -17,6 +17,20 @@ public class PlayerEndLevelCtrl {
 	}
 	
 	public void endLevel() {
-		JOptionPane.showMessageDialog(new PlayerEndLevelView(app, model), "Level Complete!");
+		String endMsg = "You won! Good job.";
+		if(model.level.currentScore < model.level.rules.scoreThresholds[0])
+			endMsg = "You didn't pass!";
+		else if(model.level.currentScore < model.level.rules.scoreThresholds[1])
+			endMsg += " 1 Star";
+		else if(model.level.currentScore < model.level.rules.scoreThresholds[2])
+			endMsg += " 2 Star!";
+		else
+			endMsg += " 3 Star!!";
+		
+		//JOptionPane.showMessageDialog(null, "Level Complete!", new PlayerEndLevelView(app, model));
+		JOptionPane jop = new JOptionPane();
+		String[] s = { "Level Select" };
+		jop.setOptions(s);
+		JOptionPane.showMessageDialog(jop, endMsg, "Level Finished", JOptionPane.PLAIN_MESSAGE);
 	}
 }
