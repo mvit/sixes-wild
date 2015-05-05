86f393955fadd315608215b620451ece3200c8d8
Maurizio Vitale <mauriziovit@gmail.com>
2015-05-05 18:58:54 -0400

    Add test case for TestBuilderLevelEditor

--- test/TestBuilderLevelEditor.java
+++ test/TestBuilderLevelEditor.java
@@ -2,25 +2,38 @@ package test;
 
 import static org.junit.Assert.*;
 
+import model.BuilderModel;
+
 import org.junit.Test;
 
 import boundary.BuilderApplication;
-
-/**
- * The builder levelEditor test class.
- *
- * @author Cem Unsal
- */
+import boundary.BuilderMainMenuView;
+import boundary.ResourceLoader;
 
 public class TestBuilderLevelEditor {
 
+	BuilderApplication app;
+	BuilderModel model;
+	ResourceLoader loader;
+	
+	
+	private void setUp() {
+	    loader = new ResourceLoader();
+	    model = new BuilderModel();
+	    app = new BuilderApplication(model, loader);
+	}
+	
+	private void tearDown() {
+		app.dispose();
+		app = null;
+	}
 	
 	@Test
 	public void test() {
-
-		BuilderApplication.main(null);
-		 
-		assertTrue (true);
+		setUp();
+		app.setView(new BuilderMainMenuView(app, model));
+		assertTrue(app.getView() instanceof BuilderMainMenuView);
+		tearDown();	
 	}
 
 }
