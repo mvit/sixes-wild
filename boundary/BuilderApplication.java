bd47a33fc3be746d15aed45a179bbf0df6369891
Maurizio Vitale <mauriziovit@gmail.com>
2015-05-05 18:58:26 -0400

    Add initialize functions to BuilderApplication

--- boundary/BuilderApplication.java
+++ boundary/BuilderApplication.java
@@ -1,9 +1,12 @@
 package boundary;
 
 import java.io.IOException;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
 import model.BuilderModel;
+import model.PlayerModel;
 
 /**
  * The builder application boundary class.
@@ -27,11 +30,82 @@ public class BuilderApplication extends JFrame {
   public BuilderApplication(BuilderModel model, ResourceLoader loader) {
     this.model = model;
     this.loader = loader;
-
-    // always exit the JVM when the user closes the window
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    SplashScreen splash = makeSplashScreen("splash2levelbuilder.png");
+    this.initialize(splash);
   }
 
+  private void initialize(SplashScreen splash) {
+	  
+	    JPanel initialView = new BuilderMainMenuView(this, model);
+	    long loadStart = System.currentTimeMillis();
+	    this.addQuitListener(null);
+	    this.initializeResources();
+	    // extend time if necessary
+	    long loadElapsed = System.currentTimeMillis() - loadStart;
+	    if (loadElapsed < 2000) {
+	      try {
+	        Thread.sleep(2000 - loadElapsed);
+	      } catch (InterruptedException err) {
+	        // if we've been interrupted, just close the splash screen
+	      } finally {
+	        // close splash screen
+	        splash.close();
+	      }
+	    }
+
+	    // start main app
+	    this.setView(initialView);
+  }
+  
+  private SplashScreen makeSplashScreen(String fname) {
+	  SplashScreen splash = null;  
+	  try {
+	        splash = new SplashScreen(fname);
+	      } catch (IOException err) {
+	        System.err.println(err.getMessage());
+	        err.printStackTrace();
+	        return null;
+	      }
+	  return splash;
+  }
+  
+  private void initializeResources() {
+	  
+	    for (int i = 1; i <= 6; i++) {
+	      loader.addResource(i + ".png");
+	    }
+	    for (int i = 2; i <= 3; i++) {
+	      loader.addResource("x" + i + ".png");
+	    }
+	    
+	    loader.addResource("playable.png");
+	    loader.addResource("selected.png");
+	    loader.addResource("marked.png");
+	    
+	    try {
+	        loader.loadResources();
+	      } catch (IOException err) {
+	        System.err.println(err.getMessage());
+	        err.printStackTrace();
+	        return;
+	      }
+  }
+  
+  /**
+   * Add a quit listener for when the user clicks the close button on the
+   * window.
+   *
+   * @param onQuit
+   */
+  public void addQuitListener(QuitListener onQuit) {
+    if (onQuit == null) {
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    } else {
+      addWindowListener(onQuit);
+    }
+  }
+
+  
   /**
    * Set the currently displayed JPanel. Makes the application visible if it's
    * not already.
@@ -64,69 +138,9 @@ public class BuilderApplication extends JFrame {
    * @param args The command-line parameters for the application.
    */
   public static void main(String[] args) {
-    // start splash screen
-    SplashScreen splash;
-    try {
-      splash = new SplashScreen("splash1levelbuilder.png");
-    } catch (IOException err) {
-      System.err.println(err.getMessage());
-      err.printStackTrace();
-      return;
-    }
-
-    long loadStart = System.currentTimeMillis();
-    BuilderModel model = new BuilderModel();
-
-    ResourceLoader loader = load();
-
-    if (loader == null) {
-      return;
-    }
-
+	ResourceLoader loader = new ResourceLoader();
+	BuilderModel model = new BuilderModel();
     // initialize main app
     BuilderApplication app = new BuilderApplication(model, loader);
-    JPanel initialView = new BuilderMainMenuView(app, model);
-
-    // extend time if necessary
-    long loadElapsed = System.currentTimeMillis() - loadStart;
-    if (loadElapsed < 2000) {
-      try {
-        Thread.sleep(2000 - loadElapsed);
-      } catch (InterruptedException err) {
-        // if we've been interrupted, just close the splash screen
-      } finally {
-        // close splash screen
-        splash.close();
-      }
-    }
-
-    // start main app
-    app.setView(initialView);
-  }
-
-  public static ResourceLoader load() {
-    ResourceLoader loader = new ResourceLoader();
-
-    for (int i = 1; i <= 6; i++) {
-      loader.addResource(i + ".png");
-    }
-    for (int i = 2; i <= 3; i++) {
-      loader.addResource("x" + i + ".png");
-    }
-
-    loader.addResource("playable.png");
-    loader.addResource("selected.png");
-    loader.addResource("marked.png");
-
-    // load resources
-    try {
-      loader.loadResources();
-    } catch (IOException err) {
-      System.err.println(err.getMessage());
-      err.printStackTrace();
-      return null;
-    }
-
-    return loader;
   }
 }
