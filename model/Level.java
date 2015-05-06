442da29fd3d6808aca50745b682cc07ce323480f
Eli Skeggs <skeggse@gmail.com>
2015-05-06 01:10:46 -0400

    Revert part of 33e4b80
    Signed-off-by: Eli Skeggs <esskeggs@wpi.edu>

--- model/Level.java
+++ model/Level.java
@@ -28,6 +28,25 @@ public class Level implements WriteStream {
   public int currentScore;
 
   /**
+   * Checks the header of the given InputStream to ensure it is a level.
+   *
+   * @param in The stream to check.
+   * @return Whether the given file could be a valid level.
+   */
+  protected static boolean checkHeader(InputStream in) throws IOException {
+    byte headerBytes[] = new byte[4];
+    if (in.read(headerBytes) != headerBytes.length) {
+      return false;
+    }
+
+    if (!header.equals(new String(headerBytes, StandardCharsets.US_ASCII))) {
+      return false;
+    }
+
+    return true;
+  }
+
+  /**
    * Gets an abstract Level constructor which reads from a DataInputStream.
    *
    * @return The readable object.
